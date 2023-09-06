9abea06c2f58e90c8569d4e09195e8e819ec2210
Paulina Gruzewska <paula@wildland.io>
2023-09-06 16:57:25 +0200

    Some changes

---
+++ SlothCreator/SlothCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlothCreatorApp.swift
+//  SlothCreator
+//
+//  Created by Paulina Grużewska on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SlothCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
